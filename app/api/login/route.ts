b8daa9910c9c414b697a6a2523b9c8acc4f10dd6
shota <sh.studylab@gmail.com>
2025-07-01 07:28:27 +0900

    api loginを一度コメントアウト

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,22 +1,22 @@
-import { supaClient } from "@/supabase/server";
-import { NextRequest, NextResponse } from "next/server";
+// import { supaClient } from "@/supabase/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  const supabase = await supaClient();
+// export async function POST(req: NextRequest) {
+//   const supabase = await supaClient();
 
-  const { email, password } = await req.json;
+//   const { email, password } = await req.json;
 
-  if (!email)
-    return NextResponse.json({ error: "emailは必須です" }, { status: 400 });
+//   if (!email)
+//     return NextResponse.json({ error: "emailは必須です" }, { status: 400 });
 
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-  if (error)
-    return NextResponse.json({ error: error.message }, { status: 500 });
+//   const { error } = await supabase.auth.signInWithPassword({
+//     email,
+//     password,
+//   });
+//   if (error)
+//     return NextResponse.json({ error: error.message }, { status: 500 });
 
-  const { data: datas, error: Err } = await supabase.auth.getUser();
-  if (Err) return NextResponse.json({ error: Err.message }, { status: 500 });
-  return NextResponse.json(datas, { status: 201 });
-}
+//   const { data: datas, error: Err } = await supabase.auth.getUser();
+//   if (Err) return NextResponse.json({ error: Err.message }, { status: 500 });
+//   return NextResponse.json(datas, { status: 201 });
+// }
